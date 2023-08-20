9b0b313d798b99ff7d1abc6845446dfc70b75053
6liyanyang <131731719+6liyanyang@users.noreply.github.com>
2023-08-20 20:46:07 +0800

    Add files via upload

---
+++ dist/js/about.70eaf24f.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunktodovue"]=self["webpackChunktodovue"]||[]).push([[443],{5399:function(t,u,n){n.r(u),n.d(u,{default:function(){return i}});var e=function(){var t=this;t._self._c;return t._m(0)},s=[function(){var t=this,u=t._self._c;return u("div",{staticClass:"about"},[u("h1",[t._v("This is an about page")])])}],a=n(1001),l={},r=(0,a.Z)(l,e,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.70eaf24f.js.map
